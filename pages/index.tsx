42218f5e300de31f52e9e4614af2555257d8754b
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-04-06 14:19:44 +0900

    feat: データ取得テスト

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,47 @@
 import React from 'react';
 import styled from 'styled-components';
+import { gql, useQuery } from '@apollo/client';
+
+interface ExampleQuery {
+  code: string;
+  name: string;
+}
+
+interface ExampleQueryDatas {
+  countries: ExampleQuery[];
+}
+
+const exampleQuery = gql`
+  query ExampleQuery {
+    countries {
+      code
+      name
+    }
+  }
+`;
 
 const Test = styled.div``;
 
 const Home = () => {
-  return <Test>indexページ</Test>;
+  const { loading, error, data } = useQuery<ExampleQueryDatas>(exampleQuery);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  return (
+    <div>
+      <header>ApolloTest</header>
+      <div>
+        {data &&
+          data.countries.map(({ code, name }) => {
+            return (
+              <div>
+                <div>country-code: {code}</div>
+                <div>country-name: {name}</div>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
