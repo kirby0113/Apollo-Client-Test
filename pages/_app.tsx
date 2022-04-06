b2cce6e37fd047c6ff5d01eceee1c0ab4633a9e2
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-04-06 14:19:35 +0900

    fix: Clientの作成をappページに変更

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,19 @@
 import { AppProps } from 'next/app';
 import 'ress';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import React from 'react';
+
+const client = new ApolloClient({
+  uri: 'https://countries.trevorblades.com',
+  cache: new InMemoryCache(),
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
     </>
   );
 }
